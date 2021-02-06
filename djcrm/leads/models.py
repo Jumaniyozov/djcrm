617ea80ba8b520139f0d0266ffcf5940dd9084e0
jumaniyozov <jumaniyozovse@gmail.com>
2021-02-06 19:18:49 +0500

    lead forms

--- djcrm/leads/models.py
+++ djcrm/leads/models.py
@@ -7,23 +7,16 @@ class User(AbstractUser):
 
 
 class Lead(models.Model):
-
-    # SOURCE_CHOICES = (
-    #     ("YouTube", "YouTube"),
-    #     ("Google", "Google"),
-    #     ("Newsletter", "Newsletter"),
-    # )
-
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
-    # phoned = models.BooleanField(default=False)
-    # source = models.CharField(choices=SOURCE_CHOICES)
-    # profile_picture = models.ImageField(blank=True, null=True)
-    # special_files = models.FileField(blank=True, null=True)
-
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
