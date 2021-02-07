fea62827950f71d35690400bcb20d5ca586a4dd3
jumaniyozov <jumaniyozovse@gmail.com>
2021-02-08 00:50:23 +0500

    categories added

--- djcrm/leads/forms.py
+++ djcrm/leads/forms.py
@@ -1,20 +1,18 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
-from .models import Lead
+from .models import Lead, Agent
 
 User = get_user_model()
 
 
 class LeadModelForm(forms.ModelForm):
     class Meta:
-
         model = Lead
         fields = (
             'first_name',
             'last_name',
             'age',
-            'organisation',
             'agent',
         )
 
@@ -30,4 +28,22 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ("username",)
         field_classes = {'username': UsernameField}
+
+
+class AssignAgentForm(forms.Form):
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop("request")
+        agents = Agent.objects.filter(organisation=request.user.userprofile)
+        super(AssignAgentForm, self).__init__(*args, **kwargs)
+        self.fields["agent"].queryset = agents
+
+
+class LeadCategoryUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = (
+            'category',
+        )
 
